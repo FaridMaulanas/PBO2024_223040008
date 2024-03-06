c4c7bbe41816d4b589e863896d0b2247d1198269
FaridMaulanas <fmaulana236@gmail.com>
2024-03-06 13:54:29 +0700

    commit pertama

---
+++ Tugas4/src/Tugas4/LingkaranBase.java
@@ -0,0 +1,20 @@
+
+package Tugas4;
+
+
+public class LingkaranBase {
+ private double jari2;
+
+    public LingkaranBase(double jari2) {
+        this.jari2 = jari2;
+    }
+
+    public double getJari2() {
+        return jari2;
+    }
+
+    public void setJari2(double jari2) {
+        this.jari2 = jari2;
+    }
+}
+
