5ad4a5ccb9196175adbe90f1a3515b13015cbe1b
FaridMaulanas <fmaulana236@gmail.com>
2024-03-28 22:55:24 +0700

    tugas 7

---
+++ Tugas7/Mahasiswa.java
@@ -0,0 +1,35 @@
+package Tugas7;
+
+public class Mahasiswa {
+	 private String NRP;
+	    private String nama;
+	    
+	    public Mahasiswa(String nrp, String nama){
+	        this.NRP = nrp;
+	        this.nama = nama;
+	    }
+	    
+	    public String display(){
+	     return "NRP: " + NRP + " Nama : " + nama; 
+	    }
+	    
+	    
+//	    SETTER DAN GETTER
+
+	    public String getNRP() {
+	        return NRP;
+	    }
+
+	    public void setNRP(String NRP) {
+	        this.NRP = NRP;
+	    }
+
+	    public String getNama() {
+	        return nama;
+	    }
+
+	    public void setNama(String nama) {
+	        this.nama = nama;
+	    }
+
+}
